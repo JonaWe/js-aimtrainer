3c735bd6fd77bf5d15b73eb6f2e262a44427eb99
Jona Wessendorf <jona.wessendorf1@uni-bielefeld.de>
2021-06-01 16:12:18 +0200

    adds update method with time delta

--- game.js
+++ game.js
@@ -3,16 +3,17 @@ class Game {
     this.canvas = document.querySelector('#game-canvas');
     this.ctx = this.canvas.getContext('2d');
 
-    this.aspectRatio = 3 / 2;
+    this.aspectRatio = 4 / 3;
 
     window.addEventListener('resize', () => this.resizeCanvas(), false);
     this.resizeCanvas();
 
-    this.helper = new Helper(this.ctx);
+    this.circle = new Circle(this.ctx, 50, 50);
 
     window.requestAnimationFrame((t) => this.gameLoop(t));
     this.lastFPSDrawingTimeStamp = 0;
     this.framesRenderedSinceLastFPSUpdate = 0;
+    this.lastUpdateTime = undefined;
     this.fps = 0;
   }
 
@@ -34,10 +35,18 @@ class Game {
 
   draw() {
     this.drawBG();
-    this.helper.drawCircle(0.25, 0.5, 0.1, 'red');
+    this.circle.draw();
+  }
+
+  update(delta) {
+    this.circle.update(delta);
   }
 
   gameLoop(timeStamp) {
+    this.lastUpdateTime ??= timeStamp;
+    this.update(timeStamp - this.lastUpdateTime);
+    this.lastUpdateTime = timeStamp;
+
     this.draw();
     this.drawFPS(timeStamp);
     window.requestAnimationFrame((t) => this.gameLoop(t));
@@ -60,22 +69,31 @@ class Game {
   }
 }
 
-class Helper {
-  constructor(ctx) {
+class Circle {
+  constructor(ctx, x, y) {
     this.ctx = ctx;
+    this.x = x;
+    this.y = y;
+    this.size = 0;
+    this.color = 'red';
   }
 
-  drawCircle(x, y, radius, color) {
+  draw() {
     this.ctx.beginPath();
     this.ctx.arc(
-      x * this.ctx.canvas.width,
-      y * this.ctx.canvas.height,
-      radius * this.ctx.canvas.height,
+      this.x * (this.ctx.canvas.width / 100),
+      this.y * (this.ctx.canvas.height / 100),
+      this.size *
+        (Math.sqrt(this.ctx.canvas.width * this.ctx.canvas.height) / 100),
       0,
       2 * Math.PI
     );
-    this.ctx.fillStyle = color;
+    this.ctx.fillStyle = this.color;
     this.ctx.fill();
+  }
+
+  update(delta) {
+    if (this.size < 20) this.size = Math.min(this.size + delta * 0.005, 20);
   }
 }
 
